6da6f473b3983df6e7322ec61fffa816e797e5d5
Richard-Xiaorui Huang <hxr.richard@gmail.com>
2023-11-10 15:02:40 -0500

    Delete reference code files

--- srcCpp/PointLight.cu
+++
@@ -1,9 +0,0 @@
-#include "PointLight.h"
-
-void PointLight::direction(
-  const Vec3d & q, Vec3d & d, double & max_t) const
-{
-    // Recall we are doing backwards tracing - from query q back to light source p
-    d = this->p - q;
-    max_t = 1;
-}
